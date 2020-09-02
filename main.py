5ba4af6e297c52a12fc8da2c4f55c3b9f13c132f
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-09-02 19:28:19 +0100

    Slap command

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import os
 import urllib.request
 import sys
@@ -8,7 +9,7 @@ import random
 from discord import File, Message
 from discord.ext import commands
 from discord.ext.commands import Bot
-version = "release1.0"
+version = "release1.1"
 try:
     prefix = os.environ['PREFIX']
     token = os.environ['TOKEN']
@@ -67,6 +68,16 @@ try:
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=arg1))
         await ctx.message.delete()
 
+    @commands.check(self_check)
+    @bot.command(pass_context=True)
+    async def slap(ctx, mention = None)
+    if mention is None:
+        mention = {bot.user}
+    await ctx.message.delete()
+    embed=discord.Embed(title=f"{bot} slaps {mention}"}, color=0xe22400)
+    embed.set_thumbnail(url="https://media1.tenor.com/images/1cf84bf514d2abd2810588caf7d9fd08/tenor.gif?itemid=7679403")
+    await ctx.send(embed=embed)
+    
     @commands.check(self_check)
     @bot.command(pass_context=True)
     async def xpbot(ctx):
