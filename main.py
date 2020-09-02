a9b8bf411b5a925d78f41889f3df7e3312622787
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-09-02 20:18:02 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import aiohttp
 import os
 import urllib.request
 import sys
@@ -303,6 +304,21 @@ try:
                 except:
                     print (f"{emoji.name} has NOT been deleted in {ctx.guild.name}")
             print ("Action Completed: dall all")
+
+    @commands.check(self_check)
+    @bot.command(pass_context=True)
+    async def avatar(ctx, *, user: discord.Member=None):
+        await ctx.message.delete()
+        format = "gif"
+        user = user or ctx.author
+        if user.is_avatar_animated() != True:
+            format = "png"
+        avatar = user.avatar_url_as(format = format if format != "gif" else None)
+        async with aiohttp.ClientSession() as session:
+            async with session.get(str(avatar)) as resp:
+                image = await resp.read()
+        with io.BytesIO(image) as file:
+            await ctx.send(file = discord.File(file, f"Avatar.{format}"))
 
     @commands.check(self_check)
     @bot.command(pass_context=True)
