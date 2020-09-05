08a959cff40bea4ac75f2d1eca16ee17d17f2177
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-09-05 23:45:01 +0100

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -99,7 +99,6 @@ class general_cog(commands.Cog):
         await ctx.send(f'I have been running for: '+uptime+'')
         embed=discord.Embed(title=f"{bot.user} Info", description="Tells you info about you. Yes you.")
         embed.add_field(name="Uptime", value=f'I have been running for: '+uptime+'', inline=False)
-        embed.add_field(name="Prefix", value=f"{prefix}", inline=True)
         embed.add_field(name="Ping", value=f"{round(self.bot.latency * 1000)}ms", inline=True)
         embed.set_footer(text="Dwifte.PY | 1.3.0")
         await ctx.send(embed=embed)
