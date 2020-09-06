4b80dabbd6207e797676b67a0c63a589d5850f3a
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-09-06 15:04:59 +0100

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,19 +9,6 @@ class general_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_through=True)
-    async def tweet(ctx, arg1 = None, arg2 = None):
-        if arg1 is None:
-            arg1 = "CrafterPika"
-        if arg2 is None:
-            arg2 = "I am amazing, trust me."
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get(f"https://nekobot.xyz/api/imagegen?type=tweet&username={arg1}&text={arg2}") as r:
-                res = await r.json()
-                em = discord.Embed()
-                em.set_image(url=res["message"])
-                await ctx.send(embed=em)
-
     @commands.command(pass_context=True, name="8ball")
     async def eigth_ball(self, ctx, arg1):
         eigth_ball_embed=discord.Embed(color=random.choice(colors), description=random.choice(answers))
