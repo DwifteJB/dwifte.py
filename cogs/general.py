f24a0552ada0033b74e4026629091a2a04a24157
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-09-06 10:53:24 +0100

    Now using {version}

--- cogs/general.py
+++ cogs/general.py
@@ -2,7 +2,7 @@ import discord
 import random
 from discord.ext import commands
 from discord import File, Message
-from config import colors
+from config import colors, version
 import sys
 import aiohttp
 import json
@@ -99,7 +99,7 @@ class general_cog(commands.Cog):
         embed.add_field(name="Uptime", value=f''+uptime+'', inline=False)
         embed.add_field(name="Prefix", value=f'{prefix}', inline=True)
         embed.add_field(name="Ping", value=f'{round(self.bot.latency * 1000)}ms', inline=True)
-        embed.set_footer(text="Dwifte.PY | 1.3.0")
+        embed.set_footer(text=f"Dwifte.PY | {version}")
         await ctx.send(embed=embed)
 
     @commands.command(pass_context=True)
