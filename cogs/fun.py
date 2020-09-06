51494098a3e0771c4dd864c02908607a4df1cbfe
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-09-06 14:52:21 +0100

    Tweet command > Fun

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,3 +1,4 @@
+import aiohttp
 import discord
 import random
 from discord.ext import commands
@@ -7,6 +8,20 @@ from config import colors, answers, kiss, kiss_description, hug, hug_description
 class general_cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(pass_through=True)
+    async def tweet(ctx, arg1 = None, arg2 = None):
+        if arg1 is None:
+            arg1 = "CrafterPika"
+        if arg2 is None:
+            arg2 = "I am amazing, trust me."
+        await ctx.message.delete()
+        async with aiohttp.ClientSession() as cs:
+            async with cs.get(f"https://nekobot.xyz/api/imagegen?type=tweet&username={arg1}&text={arg2}") as r:
+                res = await r.json()
+                em = discord.Embed()
+                em.set_image(url=res["message"])
+                await ctx.send(embed=em)
 
     @commands.command(pass_context=True, name="8ball")
     async def eigth_ball(self, ctx, arg1):
