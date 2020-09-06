16d4709e89a91799ca4cda30c6676400d6a862b3
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-09-06 13:36:08 +0100

    Removed copying due to errors :/

--- cogs/general.py
+++ cogs/general.py
@@ -44,24 +44,6 @@ class general_cog(commands.Cog):
         embed.set_footer(text=arg3)
         await ctx.send(embed=embed)
         print ("Action Completed: say")
-
-    @commands.command(pass_context=True)
-    async def backup(ctx): 
-        await ctx.message.delete()
-        await bot.create_guild(f'backup-{ctx.guild.name}')
-        await asyncio.sleep(4)
-        for g in bot.guilds:
-            if f'backup-{ctx.guild.name}' in g.name:
-                for c in g.channels:
-                    await c.delete()
-                for cate in ctx.guild.categories:
-                    x = await g.create_category(f"{cate.name}")
-                    for chann in cate.channels:
-                        if isinstance(chann, discord.VoiceChannel):
-                            await x.create_voice_channel(f"{chann}")
-                        if isinstance(chann, discord.TextChannel):
-                            await x.create_text_channel(f"{chann}")
-
     @commands.command(pass_context=True)
     async def playing(self, ctx, arg1):
         await self.bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name=arg1))
