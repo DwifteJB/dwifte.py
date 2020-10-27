b17651e030a7adc1374e1060b315258d1fe2888b
DwifteJB <67983181+DwifteJB@users.noreply.github.com>
2020-10-27 23:24:33 +0000

    smort

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-print("Removing files except config")
+echo "Removing files except config"
 rm -r -f *.py *.txt *.sh cogs data *.md app.json update.json
 git clone https://github.com/DwifteJB/dwifte.py temp
 rm -f temp/config.json
