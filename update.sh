39722cdee4be91e90e407d7eebe08d4e49d357a7
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-01-10 11:17:59 +0000

    .

--- update.sh
+++
@@ -1,5 +0,0 @@
-echo "Removing files except config"
-rm -r -f *.py *.txt *.sh cogs data *.md app.json update.json
-git clone https://github.com/DwifteJB/dwifte.py temp
-rm -f temp/config.json
-mv temp/* ./
