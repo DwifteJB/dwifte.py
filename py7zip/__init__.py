eecff19d2b9df7f0fb6b826905174663270e6313
CrafterPika <48294025+CrafterPika@users.noreply.github.com>
2021-01-11 17:25:43 +0000

    .

--- py7zip/__init__.py
+++ py7zip/__init__.py
@@ -1,9 +1,6 @@
 import os
 import subprocess
 import shlex
-from dotenv import load_dotenv
-
-load_dotenv()
 
 """
 **7zip is not created by me. 7zip belongs to Igor Pavlov**
@@ -13,7 +10,7 @@ py7zip
 Compress/Extract files ot .7z
 """
 
-__version__ = "0.1"
+__version__ = "1.0"
 __author__ = 'CrafterPika'
 __credits__ = 'https://www.7-zip.org/, Igor Pavlov'
 
@@ -25,20 +22,22 @@ class compress():
         pass
 
     # make_archive
-    def archive_file(self, file, name):
+    def archive_file(self, file, name, password = None):
         module_path = os.path.dirname(__file__)
-        bin = os.getenv("7ZIP_BIN")
-        cli7z = f"{module_path}{bin}"
-        #print(cli7z)
-        subprocess.run(shlex.split(f'"{cli7z}" a -t7z "{os.getcwd()}/{name}.7z" "{os.getcwd()}/{file}" -mx=9'))
+        cli7z = f"{module_path}/bin/7z.exe"
+        if password == None:
+            subprocess.run(shlex.split(f'"{cli7z}" a -t7z "{os.getcwd()}/{name}.7z" "{os.getcwd()}/{file}" -mx=9 -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        else:
+            subprocess.run(shlex.split(f'"{cli7z}" a -t7z "-p{password}" "{os.getcwd()}/{name}.7z" "{os.getcwd()}/{file}" -mx=9 -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     # folder archiving
-    def archive_folder(self, folder, name):
+    def archive_folder(self, folder, name, password = None):
         module_path = os.path.dirname(__file__)
-        bin = os.getenv("7ZIP_BIN")
-        cli7z = f"{module_path}{bin}"
-        #print(cli7z)
-        subprocess.run(shlex.split(f'"{cli7z}" a -t7z "{os.getcwd()}/{name}.7z" "{os.getcwd()}/{folder}/*" -mx=9'))
+        cli7z = f"{module_path}/bin/7z.exe"
+        if password == None:
+            subprocess.run(shlex.split(f'"{cli7z}" a -t7z "{os.getcwd()}/{name}.7z" "{os.getcwd()}/{folder}/*" -mx=9 -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        else:
+            subprocess.run(shlex.split(f'"{cli7z}" a -t7z "-p{password}" "{os.getcwd()}/{name}.7z" "{os.getcwd()}/{folder}/*" -mx=9 -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 class extract():
 
@@ -47,15 +46,30 @@ class extract():
         pass
 
     # extract to root folder
-    def extract(self, file):
+    def extract(self, file, password = None):
         module_path = os.path.dirname(__file__)
-        bin = os.getenv("7ZIP_BIN")
-        cli7z = f"{module_path}{bin}"
-        subprocess.run(shlex.split(f'"{cli7z}" e "{os.getcwd()}/{file}.7z"'))
+        cli7z = f"{module_path}/bin/7z.exe"
+        if password == None:
+            subprocess.run(shlex.split(f'"{cli7z}" e "{os.getcwd()}/{file}.7z" -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        else:
+            subprocess.run(shlex.split(f'"{cli7z}" e "-p{password}" "{os.getcwd()}/{file}.7z" -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
     # extract to folder name
-    def extract_folder(self, file, name):
+    def extract_folder(self, file, name, password = None):
         module_path = os.path.dirname(__file__)
-        bin = os.getenv("7ZIP_BIN")
-        cli7z = f"{module_path}{bin}"
-        subprocess.run(shlex.split(f'"{cli7z}" e "{os.getcwd()}/{file}.7z" "-o{os.getcwd()}/{name}"'))
+        cli7z = f"{module_path}/bin/7z.exe"
+        if password == None:
+            subprocess.run(shlex.split(f'"{cli7z}" e "{os.getcwd()}/{file}.7z" "-o{os.getcwd()}/{name}" -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        else:
+            subprocess.run(shlex.split(f'"{cli7z}" e "-p{password}" "{os.getcwd()}/{file}.7z" "-o{os.getcwd()}/{name}" -y'), shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+class sfx():
+
+    #initial def
+    def __init__(self):
+        pass
+
+    def creat(self, folder, name):
+        module_path = os.path.dirname(__file__)
+        cli7z = f"{module_path}/bin/7z.exe"
+        subprocess.run(shlex.split(f'"{cli7z}" a -t7z -mx5 -sfx "{name}.exe" "{os.getcwd()}/{folder}/*" -mx=9 -y'), shell=True)
--- py7zip/examples/example.py
+++ py7zip/examples/example.py
@@ -7,9 +7,21 @@ file.archive_file("example.txt", "example_compressed")
 file = compress()
 file.archive_folder("example_folder", "example_folder_compressed")
 
-#extract
+# compress with password
+file = compress()
+file.archive_file("example.txt", "example_pass", "example")
+
+# extract
 file = extract()
 file.extract("example_compressed")
 
 file = extract()
 file.extract_folder("example_folder_compressed", "example_out")
+
+# extracting with password
+file = extract()
+file.extract("example_pass", "example")
+
+# creat sfx archive
+file = sfx()
+file.creat("example", "example_sfx")
